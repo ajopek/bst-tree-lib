516b5bc886aff62d3622765eb9a06c269d03e242
fawq <krystian.adr@wp.pl>
2018-01-26 13:47:19 +0100

    Increase BSTree unit test coverage

--- test/BSTreeTests/UnitTests.hs
+++ test/BSTreeTests/UnitTests.hs
@@ -16,6 +16,13 @@ validTree =
     (Node (Leaf) 7 (Leaf))
     )
 
+anotherTree =
+    (Node
+    (Node (Leaf) 3 (Leaf))
+    6
+    (Node (Leaf) 8 (Leaf))
+    )
+
 invalidTree =
     (Node
     (Node (Leaf) 3 (Leaf))
@@ -23,12 +30,33 @@ invalidTree =
     (Node (Leaf) 7 (Leaf))
     )
 
+
 test_isValidValidTree = TestCase (do
   assertEqual "Valid tree is not deteced as valid" True (isValid validTree)
   )
 
 test_isValidInvalidTree = TestCase (do
   assertEqual "Invalid tree is not deteced as invalid" False (isValid invalidTree)
+  )
+
+test_containFive = TestCase (do
+  assertEqual "Tree with key equal 5 return false" True (conatins validTree 5)
+  )
+
+test_minKey = TestCase (do
+  assertEqual "Minimum key in tree is not equal 3" 3 (leftBST validTree)
+  )
+
+test_maxKey = TestCase (do
+  assertEqual "Maximum key in tree is not equal 7" 7 (rightBST validTree)
+  )
+
+test_compareOfTrees = TestCase (do
+  assertEqual "Diffrent trees are equal" False (compareBST validTree anotherTree)
+  )
+
+test_heightTree = TestCase (do
+  assertEqual "Height of tree is not equal 2" 2 (heightBST validTree)
   )
 
 unitTests = TestList [test_isValidValidTree, test_isValidInvalidTree]
